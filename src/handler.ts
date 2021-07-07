8552534bd6c148b51bd7cc15ca5c37778677d354
Alejo Mendoza <alejandro@skyhitzmusic.com>
2021-07-07 12:46:51 -0500

    run command line jest

--- src/handler.ts
+++ src/handler.ts
@@ -1,13 +1,28 @@
 import { APIGatewayProxyEvent } from 'aws-lambda';
 import { performance } from 'perf_hooks';
+const exec = require('child_process').exec;
+
+function runTests() {
+  return new Promise(function (resolve, reject) {
+    exec('jest', (error: any, stdout: any, stderr: any) => {
+      if (error) {
+        return resolve({ statusCode: 500, body: error.message });
+      }
+      if (stderr) {
+        return resolve({ statusCode: 500, body: stderr });
+      }
+      return resolve({ statusCode: 200, body: stdout });
+    });
+  });
+}
 
 const handler = async (event: APIGatewayProxyEvent) => {
   const time1 = performance.now();
-  // const response = await runTests();
+  const response = await runTests();
   const time2 = performance.now();
   console.log(time2 - time1);
 
-  return {};
+  return response;
 };
 
 export default handler;
--- src/web.test.ts
+++ src/web.test.ts
@@ -1,8 +1,10 @@
 const slsw = require('serverless-webpack');
+import { beforeAll, test, afterAll } from '@jest/globals';
+import { Browser, Page } from 'puppeteer';
 let chromium: any;
 let puppeteer: any;
-let browser: any;
-let page: any;
+let browser: Browser;
+let page: Page;
 
 if (slsw.lib.webpack.isLocal) {
   puppeteer = require('puppeteer');
@@ -13,7 +15,16 @@ if (slsw.lib.webpack.isLocal) {
 
 async function launchPuppeteer() {
   if (slsw.lib.webpack.isLocal) {
-    browser = await puppeteer.launch();
+    if (!browser) {
+      browser = await puppeteer.launch({
+        args: [
+          '--no-sandbox',
+          '--disable-setuid-sandbox',
+          '--disable-dev-shm-usage',
+        ],
+        headless: true,
+      });
+    }
   } else {
     browser = await puppeteer.launch({
       args: chromium.args,
@@ -23,18 +34,21 @@ async function launchPuppeteer() {
       ignoreHTTPSErrors: true,
     });
   }
+}
+
+async function openIncognitoWindow() {
   page = await browser.newPage();
   await page.goto(SITE_URL);
 }
 
 const SITE_URL = 'https://sq-royale-test.vercel.app/';
 
-// export async function runTests() {
 beforeAll(async () => {
   await launchPuppeteer();
+  await openIncognitoWindow();
 });
 
-test('Renders Stellar Quest Logo', async () => {
+test('Renders Home Page', async () => {
   await page.waitForSelector('#root');
 
   const header = await page.$eval(
@@ -44,7 +58,46 @@ test('Renders Stellar Quest Logo', async () => {
   expect(header).toBe(`Stellar Quest`);
 });
 
+test('Renders Sign Up Page', async () => {
+  await page.waitForSelector('#root');
+
+  await Promise.all([
+    page.click('[data-testid="signupBtn"]'),
+    page.waitForNavigation(),
+  ]);
+
+  const header = await page.$eval('header>h1', (e: any) => e.innerHTML);
+  expect(header).toBe(`Sign Up`);
+});
+
+// test('Renders Rules Page', async () => {
+//   await page.waitForSelector('#root');
+
+//   await Promise.all([
+//     page.click('[data-testid="rulesBtn"]'),
+//     page.waitForNavigation(),
+//   ]);
+
+//   const header = await page.$eval(
+//     'header>div>h1>span',
+//     (e: any) => e.innerHTML
+//   );
+//   expect(header).toBe(`Stellar Quest`);
+// });
+
+// test('Opens Discord Auth Page', async () => {
+//   await page.waitForSelector('#root');
+
+//   await Promise.all([
+//     page.click('[data-testid="loginBtn"]'),
+//     page.waitForNavigation(),
+//   ]);
+
+//   const [tabOne, tabTwo] = await browser.pages();
+
+//   expect(tabTwo).toBeTruthy();
+// });
+
 afterAll(() => {
   browser.close();
 });
-// }
